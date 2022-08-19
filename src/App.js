def891680bd48d847bc51790e3cb80343e150df8
Erick Costa Barbosa <cc20342@g.unicamp.br>
2022-08-19 16:23:47 -0300

    second commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Logo from './components/template/Logo';
+import Menu from './components/template/Menu';
+import Footer from './components/template/Footer';
+import { BrowserRouter } from 'react-router-dom';
+
+import Rotas from './Rotas';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Logo />
+        <Menu />
+        <Rotas/>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
+export default App;
---
+++ src/Rotas.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+
+import Main from './components/template/Main';
+import CrudAluno from './components/CrudAluno';
+
+export default function Rotas() {
+    return (
+    <Routes>
+        <Route exact path='/'
+            element={
+                <Main title="Bem Vindo!">
+                    <div>Cadastro de alunos, cursos e carômetro</div>
+                </Main>
+            }
+        />
+        <Route path='/alunos' element={<CrudAluno />} />
+
+        
+
+    </Routes>
+    )
+}
