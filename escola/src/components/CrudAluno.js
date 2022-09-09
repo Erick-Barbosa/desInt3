8ce972a110e6aeeb511526ef1c17e34faaded09a
ERICK COSTA BARBOSA <u20342@LAPA22.academico.cotuca.unicamp.br>
2022-09-09 19:56:10 -0300

    Cotuca - pdf 5

--- escola/src/components/CrudAluno.js
+++ escola/src/components/CrudAluno.js
@@ -1,17 +1,106 @@
 import './CrudAluno.css';
 import React, { Component } from 'react';
 import Main from './template/Main';
+import axios from 'axios'
 
 const title = "Cadastro de Alunos";
 
-const Alunos = [
-    { 'id': 1, 'ra': 11111, 'nome': 'André', 'codCurso': 19 },
-    { 'id': 2, 'ra': 22222, 'nome': 'Amanda', 'codCurso': 28 },
-    { 'id': 3, 'ra': 33333, 'nome': 'Pedro', 'codCurso': 39 },
-    { 'id': 4, 'ra': 44444, 'nome': 'Alice', 'codCurso': 59 },
-];
+const urlApi = "http://localhost:5041/api/aluno";
+const initialState = {
+    aluno: {id: 0, ra: '', nome: '', codCurso: ''},
+    lista: []
+}
 
 export default class CrudAluno extends Component {
+
+    state = {...initialState}
+
+    componentDidMount() {
+        axios(urlApi).then(resp => {
+            this.setState({ lista: resp.data })
+        })
+    }
+
+    limpar() {
+        this.setState({ aluno: initialState.aluno });
+    }
+
+    salvar() {
+        const aluno = this.state.aluno;
+        aluno.codCurso = Number(aluno.codCurso);
+        const metodo = 'post';
+
+        axios[metodo](urlApi, aluno)
+            .then(resp => {
+                const lista = this.getListaAtualizada(resp.data)
+                this.setState({ aluno: initialState.aluno, aluno})
+            })
+    }
+
+    getListaAtualizada(aluno) {
+        const lista = this.state.lista.filter(a => a.id !== aluno.id);
+        lista.unshift(aluno);
+        return lista;
+    }
+
+    atualizaCampo(event) {
+        // clonar usuário a partir do state, para não alterar o state diretamente
+        const aluno = { ...this.state.aluno };
+        // usar o atributo NAME do input identificar o campo a ser atualizado
+        aluno[event.target.name] = event.target.value;
+        // atualizar o state
+        this.setState({ aluno })
+    }
+
+    renderForm() {
+        return (
+            <div className="inclui-container">
+                <label> RA: </label>
+                <input
+                    type="text"
+                    id="ra"
+                    placeholder="RA do aluno"
+                    className="form-input"
+                    name="ra"
+                    
+                    value={this.state.aluno.ra}
+                    
+                    onChange={ e => this.atualizaCampo(e)}
+                />
+                <label> Nome: </label>
+                <input
+                    type="text"
+                    id="nome"
+                    placeholder="Nome do aluno"
+                    className="form-input"
+                    name="nome"
+                    
+                    value={this.state.aluno.nome}
+                    
+                    onChange={ e => this.atualizaCampo(e)}
+                />
+                <label> Código do Curso: </label>
+                <input
+                    type="number"
+                    id="codCurso"
+                    placeholder="0"
+                    className="form-input"
+                    name="codCurso"
+                    
+                    value={this.state.aluno.codCurso}
+                    onChange={ e => this.atualizaCampo(e)}
+                />
+                <button className="btnSalvar"
+                    onClick={e => this.salvar(e)} >
+                        Salvar
+                </button>
+                <button className="btnCancelar"
+                    onClick={e => this.limpar(e)} >
+                        Cancelar
+                </button>
+            </div>
+        )
+    }
 
     renderTable() {
         return (
@@ -25,7 +114,7 @@ export default class CrudAluno extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {Alunos.map(
+                        {this.state.lista.map(
                             (aluno) =>
                             <tr key={aluno.id}>
                                 <td>{aluno.ra}</td>
@@ -44,6 +133,7 @@ export default class CrudAluno extends Component {
     render() {
         return (
             <Main title={title}>
+                {this.renderForm()}
                 {this.renderTable()}
             </Main>
         )
